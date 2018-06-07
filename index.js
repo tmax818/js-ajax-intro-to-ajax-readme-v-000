8c85c6f305b28a0166e7f01f70d6c58a22e3fd1d
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-06-07 15:18:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function showRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText)
+}
+ 
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
