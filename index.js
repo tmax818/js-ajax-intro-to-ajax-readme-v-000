683889cbaf02e9ec6cee80f9c146b643b5a312bb
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-08-23 14:57:34 -0700

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getRepositories() {
   const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
@@ -8,19 +9,20 @@ function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText)
 }
- 
-function getRepositories() {
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
-  req.send()
-}
 
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
 }
 
 function showCommits() {
